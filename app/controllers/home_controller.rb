3bbaf094de6940d8fd88210d1c6108b7e78a5a0a
mboker <bbokerr6@gmail.com>
2013-11-25 17:37:09 -0500

    Changed alert styling for actions that are not allowed. Also fixed a bug that was causing seats to not decrement when a rider joined a ride

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @rides = Ride.all
+    @rides_with_open_seats = Ride.where("seats > ?", 0)
+    #@rides_needing_drivers = Ride.where(
   end
 end
